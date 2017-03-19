a9c706eb4d4ee93a805f6d6f11ad8a0d6eb3d343
devinfos <devinfos@yahoo.com>
2017-03-19 13:55:09 +0300

    Added getters and setters, constructor and deleted studentId and changed studentClass to form

--- src/main/java/org/dev/pro/model/Student.java
+++ src/main/java/org/dev/pro/model/Student.java
@@ -5,8 +5,45 @@ package org.dev.pro.model;
 public class Student{
 private int id;
 private String name;
-private int studentId;
-private int studentClass;
+private int form;
 private int status;
 
+public Student(int id, String name, int form, int status){
+this.id=id;
+this.name=name;
+this.form=form;
+this.status=status;
+}
+
+public int getId(){
+return id;
+}
+
+public void setId(int id){
+this.id=id;
+}
+
+public String getName(){
+return name;
+}
+
+public void setName(String name){
+this.name=name;
+}
+
+public int getForm(){
+return form;
+}
+
+public void setForm(int form){
+this.form=form;
+}
+
+public int getStatus(){
+return this.status;
+}
+
+public void setStatus(int status){
+this.status=status;
+}
 }
